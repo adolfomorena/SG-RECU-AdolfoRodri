4f1061df40325b85d591b623464f94e852b16f6d
adolfomorena <adomorang@gamil.com>
2025-04-01 12:05:52 +0200

    creacion del modelo

---
+++ SG-RECU-AdolfoRodri/MVVM/Models/Tarea.cs
@@ -0,0 +1,27 @@
+﻿using PropertyChanged;
+using SGTAREASAdolfo.Abstractions;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_RECU_AdolfoRodri.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    [Table("Tareas")]
+    
+    public class Tarea: TableData
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public bool Estado { get; set; } = false;
+        public string Prioridad { get; set; } = string.Empty;
+
+        //[ManyToMany(CascadeOperations = CascadeOperation.All)]
+        public ObservableCollection<Etiqueta> Etiquetas { get; set; } = new ObservableCollection<Etiqueta>();
+    }
+}

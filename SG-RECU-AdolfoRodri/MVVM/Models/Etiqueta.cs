574a34ff3bfb17d67fcaf2b5d832bef3fdef0fcd
RodriGitTS <127401631+RodriGitTS@users.noreply.github.com>
2025-04-07 23:35:17 +0200

    Modelo Etiquetas creado

---
+++ SG-RECU-AdolfoRodri/MVVM/Models/Etiqueta.cs
@@ -0,0 +1,25 @@
+﻿using PropertyChanged;
+using SGTAREASAdolfo.Abstractions;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_RECU_AdolfoRodri.MVVM.Models
+{
+    
+    [Table("Etiquetas")]
+    public class Etiqueta : TableData
+    {
+        [Unique]
+        public string Nombre { get; set; } = string.Empty;
+
+
+        //[ManyToMany(typeof(Etiqueta), CascadeOperations = CascadeOperation.All)]
+        public ObservableCollection<Tarea> tareas { get; set; } = new ObservableCollection<Tarea> { };
+    }
+}

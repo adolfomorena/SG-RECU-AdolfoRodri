e0ae96e4553f96263fbd2ddc84942be617d59316
adolfomorena <adomorang@gamil.com>
2025-04-08 09:10:29 +0200

    inicializacion repositorios

--- SG-RECU-AdolfoRodri/App.xaml.cs
+++ SG-RECU-AdolfoRodri/App.xaml.cs
@@ -1,12 +1,20 @@
-﻿namespace SG_RECU_AdolfoRodri
+﻿using SG_RECU_AdolfoRodri.MVVM.Models;
+using SG_RECU_AdolfoRodri.MVVM.Views;
+using SQLiteEjemplo.Repositories;
+
+namespace SG_RECU_AdolfoRodri
 {
     public partial class App : Application
     {
-        public App()
+        public static BaseRepository<Tarea> TareaRepo { get; private set; }
+        public static BaseRepository<Etiqueta> EtiquetaRepo { get; private set; }
+        public App(BaseRepository<Tarea> objTareaRepo, BaseRepository<Etiqueta> objEtiquetaRepo)
         {
+            //TareaRepo = objTareaRepo;
+            //EtiquetaRepo = objEtiquetaRepo;
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new ListaTareasView());
         }
     }
 }

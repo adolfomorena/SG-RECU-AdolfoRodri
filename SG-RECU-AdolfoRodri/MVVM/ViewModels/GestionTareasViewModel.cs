8e962ef92944afa6e2a6fb60bff9f74714780475
adolfomorena <adomorang@gamil.com>
2025-04-23 18:35:51 +0200

    funcionalidad botones

--- SG-RECU-AdolfoRodri/MVVM/ViewModels/GestionTareasViewModel.cs
+++ SG-RECU-AdolfoRodri/MVVM/ViewModels/GestionTareasViewModel.cs
@@ -16,16 +16,48 @@ namespace SG_RECU_AdolfoRodri.MVVM.ViewModels
         public Tarea TareaSeleccionada { get; set; }
         public ObservableCollection<Etiqueta> EtiquetasDisponibles { get; set; } = new ObservableCollection<Etiqueta>();
 
+        public ICommand VolverCommand { get; private set; }
+        public ICommand GuardarCommand { get; private set; }
+        public ICommand GestionEtiquetasCommand { get; private set; }
+
         public GestionTareasViewModel(Tarea tarea)
         {
             TareaSeleccionada = tarea;
+            VolverCommand = new Command(Volver);
+            GuardarCommand = new Command(Guardar);
+            GestionEtiquetasCommand = new Command(GestionEtiquetas);
             RefreshView();
         }
         public GestionTareasViewModel()
         {
             TareaSeleccionada = new Tarea();
+            VolverCommand = new Command(Volver);
+            GuardarCommand = new Command(Guardar);
+            GestionEtiquetasCommand = new Command(GestionEtiquetas);
             RefreshView();
         }
+
+        private void Volver()
+        {
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        // can execute y guardar bien las etiquetas
+        private void Guardar()
+        {
+            App.TareaRepo.SaveItemCascada(TareaSeleccionada);
+            App.Current.MainPage.DisplayAlert("Creada", "Etiqueta creada correctamente", "Ok");
+
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+        private void GestionEtiquetas()
+        {
+            App.Current.MainPage.Navigation.PushAsync(new GestionEtiquetasView
+            {
+                BindingContext = new GestionEtiquetasViewModel()
+            });
+        }
+
         private void RefreshView()
         {
             EtiquetasDisponibles = new ObservableCollection<Etiqueta>(App.EtiquetaRepo.GetItems());
